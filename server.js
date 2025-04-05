cb80692a3e2e38d167e7da26944e638c9cf7832e
RAVI <johnnyrooks4554@gmail.com>
2025-04-05 09:50:12 +0530

    Add server.js

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ mongoose.connect(mongoURI)
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://incredible-jelly-b0d9ae.netlify.app/',  // Replace with your frontend URL in production
+  methods: 'GET,POST,PUT,DELETE',
+}));
 app.use(express.json());  // To parse JSON bodies
 
 // Routes
